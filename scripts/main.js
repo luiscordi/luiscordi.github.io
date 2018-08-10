f884d6b971ed84bb0ffb82351c4e2c75205c76cd
luiscordi <luiscordi@gmail.com>
2018-08-09 23:15:04 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,14 +18,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'Programing is cool, ' + myName;
 }	
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Programing is cool, ' + storedName;
 }
 
 myButton.onclick = function() {
