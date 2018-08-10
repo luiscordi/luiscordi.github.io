21cd929fb908937caade17b525b4f8ddc56f7ec0
luiscordi <luiscordi@gmail.com>
2018-08-09 23:12:35 -0300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+var myImage = document.querySelector('img');
+
+//image changer function
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/hello.jpg') {
+      myImage.setAttribute ('src','images/helloEye.jpg');
+    } else {
+      myImage.setAttribute ('src','images/hello.jpg');
+    }
+}
+
+
+//Change user code
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}	
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
